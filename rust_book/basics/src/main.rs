d002128ba2a6ddbac137a36bcb0c46b03e127f26
Josh Reuben <jreuben111@gmail.com>
2023-03-05 18:05:02 +0200

    modules complete

--- rust_book/basics/src/main.rs
+++ rust_book/basics/src/main.rs
@@ -1,9 +1,22 @@
-use crate::module1::submodule1::Dummy;
 
-pub mod module1;
+
+pub mod module1; // looks for file as not inline 
+
 
 fn main() {
-    
-    let d = Dummy{};
-    println!("Hello, {:?}!", d);
+    crate::modules::f(); //CH7
 }
+
+mod modules {
+    use crate::module1::submodule1::Dummy;
+    use basics::customer; // prefix with package name as it is in lib.rs
+
+    pub fn f() {
+        let d = Dummy{};
+        println!("Hello, {:?}!", d);
+
+        customer::eat_at_restaurant();
+    }
+}
+
+
