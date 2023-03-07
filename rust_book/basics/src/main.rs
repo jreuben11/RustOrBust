d40a07b23a133a0e3d07ffc9b11b3e3dfc0667e1
Josh Reuben <jreuben111@gmail.com>
2023-03-07 17:39:34 +0200

    CH05

--- rust_book/basics/src/main.rs
+++ rust_book/basics/src/main.rs
@@ -1,5 +1,6 @@
 
-
+// cargo fix --lib -p basics     
+// cargo fix --bin "basics"  
 
 pub mod module1; // looks for file as not inline 
 
@@ -15,6 +16,8 @@ fn main() {
     ownership::ownership_examples();
     ownership::borrowing();
     ownership::slices();
+
+    structs::use_structs();
 }
 
 mod modules { // CH07
@@ -233,4 +236,87 @@ mod ownership { //CH04
         }
         &s[..]
     }
+}
+
+mod structs { //CH05
+
+    #[derive(Debug)]
+    struct User {
+        active: bool,
+        username: String,
+        email: String,
+        sign_in_count: u64,
+    }
+    struct Point(i32, i32, i32); // tuple struct
+    struct AlwaysEqual; // unit-like struct
+
+    #[derive(Debug)]
+    struct Rectangle {
+        width: u32,
+        height: u32,
+    }
+    impl Rectangle {
+        fn area(&self) -> u32 {
+            self.width * self.height
+        }
+        fn can_hold(&self, other: &Rectangle) -> bool {
+            self.width > other.width && self.height > other.height
+        }
+        fn square(size: u32) -> Self { // associated function
+            Self {
+                width: size,
+                height: size,
+            }
+        }
+    }
+
+    pub fn use_structs(){
+        let mut user1 = User {
+            active: true,
+            username: String::from("someusername123"),
+            email: String::from("someone@example.com"),
+            sign_in_count: 1,
+        };
+        user1.email = String::from("anotheremail@example.com");
+
+        let user2 = build_user("blah".to_string(), "blah".to_string());
+        let user3 = User {
+            email: String::from("another@example.com"),
+            ..user2 // struct update syntax
+        };
+        println!("{}-{}-{}", user3.active, user3.username, user3.sign_in_count);
+
+        let _origin = Point(0, 0, 0);
+        let _subject = AlwaysEqual;
+
+        let rect1 = Rectangle {
+            width: 30,
+            height: 50,
+        };
+        println!(
+            "The area of the rectangle is {} square pixels.",
+            rect1.area()
+        );
+        println!("rect1 is {:?}", rect1);
+        println!("rect1 is {:#?}", rect1);
+        dbg!(&rect1);
+
+        let rect2 = Rectangle {
+            width: 60,
+            height: 45,
+        };
+        println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+
+        let _square = Rectangle::square(10);
+
+    }
+    fn build_user(email: String, username: String) -> User {
+        User {
+            active: true,
+            username, // field init shorthand
+            email,
+            sign_in_count: 1,
+        }
+    }
+    
 }
