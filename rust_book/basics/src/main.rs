418403158db0e2bc21ce91a26f2b72cb08f02d6f
Josh Reuben <jreuben111@gmail.com>
2023-03-08 18:12:39 +0200

    CH10

--- rust_book/basics/src/main.rs
+++ rust_book/basics/src/main.rs
@@ -26,6 +26,10 @@ fn main() {
     common_collections::hashmaps();
 
     error_handling::dont_panic();
+
+    generics::data_types();
+    generics::traits();
+    generics::lifetimes();
 }
 
 mod modules { // CH07
@@ -529,4 +533,160 @@ mod error_handling { //CH09
             .parse()
             .expect("Hardcoded IP address should be valid");
     }
+}
+
+mod generics { //CH10
+
+    pub fn data_types(){
+        let number_list = vec![34, 50, 25, 100, 65];
+        let result = largest(&number_list);
+        println!("The largest number is {}", result);
+    
+        let char_list = vec!['y', 'm', 'a', 'q'];
+        let result = largest(&char_list);
+        println!("The largest char is {}", result);
+
+        fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+            let mut largest = &list[0];
+            for item in list {
+                if item > largest {
+                    largest = item;
+                }
+            }
+            largest
+        }
+        struct Point<T> {
+            x: T,
+            y: T,
+        }
+        impl<T> Point<T> {
+            fn x(&self) -> &T {
+                &self.x
+            }
+        }
+        impl Point<f32> {
+            fn distance_from_origin(&self) -> f32 {
+                (self.x.powi(2) + self.y.powi(2)).sqrt()
+            }
+        }
+        let integer = Point { x: 5, y: 10 };
+        let float = Point { x: 1.0, y: 4.0 };
+        println!("p.x = {}", integer.x());
+        println!("p.distance_from_origin = {}", float.distance_from_origin());
+    }
+    pub fn traits(){
+        pub trait Summary {
+            fn summarize(&self) -> String;
+            fn summarize2(&self) -> String {
+                format!("(Read more from {}...)", self.summarize())
+            }
+        }
+        pub struct NewsArticle {
+            pub headline: String,
+            pub location: String,
+            pub author: String,
+            pub content: String,
+        }
+        
+        impl Summary for NewsArticle {
+            fn summarize(&self) -> String {
+                format!("{}, by {} ({})", self.headline, self.author, self.location)
+            }
+        }
+        
+        pub struct Tweet {
+            pub username: String,
+            pub content: String,
+            pub reply: bool,
+            pub retweet: bool,
+        }
+        
+        impl Summary for Tweet {
+            fn summarize(&self) -> String {
+                format!("{}: {}", self.username, self.content)
+            }
+        }
+
+        let tweet = Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from(
+                "of course, as you probably already know, people",
+            ),
+            reply: false,
+            retweet: false,
+        };
+    
+        println!("1 new tweet: {}", tweet.summarize());
+        println!("blah {}", tweet.summarize2());
+
+        pub fn notify(item: &impl Summary) {
+            println!("Breaking news! {}", item.summarize());
+        }
+
+        fn returns_summarizable() -> impl Summary {
+            Tweet {
+                username: String::from("horse_ebooks"),
+                content: String::from(
+                    "of course, as you probably already know, people",
+                ),
+                reply: false,
+                retweet: false,
+            }
+        }
+        let t = returns_summarizable();
+        notify(&t);
+    }
+
+    pub fn lifetimes() {
+        let string1 = String::from("abcd");
+        let string2 = "xyz";
+    
+        let result = longest(string1.as_str(), string2);
+        println!("The longest string is {}", result);
+
+        fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+            if x.len() > y.len() {
+                x
+            } else {
+                y
+            }
+        }
+
+        struct ImportantExcerpt<'a> {
+            part: &'a str,
+        }
+        let novel = String::from("Call me Ishmael. Some years ago...");
+        let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+        let i = ImportantExcerpt {
+            part: first_sentence,
+        };
+
+        impl<'a> ImportantExcerpt<'a> {
+            fn announce_and_return_part(&self, announcement: &str) -> &str {
+                println!("Attention please: {}", announcement);
+                self.part
+            }
+        }
+
+        let s: &'static str = "I have a static lifetime.";
+
+        use std::fmt::Display;
+
+        fn longest_with_an_announcement<'a, T>(
+            x: &'a str,
+            y: &'a str,
+            ann: T,
+        ) -> &'a str
+        where
+            T: Display,
+        {
+            println!("Announcement! {}", ann);
+            if x.len() > y.len() {
+                x
+            } else {
+                y
+            }
+        }
+    }
+    
 }
