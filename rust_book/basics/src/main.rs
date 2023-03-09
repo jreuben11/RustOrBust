627454ab3d72ae5e9eba09df3c36331e1839ae4d
Josh Reuben <jreuben111@gmail.com>
2023-03-09 07:08:47 +0200

    unit tests

--- rust_book/basics/src/main.rs
+++ rust_book/basics/src/main.rs
@@ -690,3 +690,102 @@ mod generics { //CH10
     }
     
 }
+
+mod testables { // for unit testing:
+    #[derive(Debug)]
+    pub struct Rectangle {
+        pub width: u32,
+        pub height: u32,
+    }
+    impl Rectangle {
+        pub fn can_hold(&self, other: &Rectangle) -> bool {
+            self.width > other.width && self.height > other.height
+        }
+    }
+    pub fn greeting(name: &str) -> String {
+        format!("Hello {}!", name)
+    }
+    pub fn dont_panic(){
+        panic!("AAAAGH!");
+    }
+    pub struct Guess {
+        value: i32,
+    }
+    impl Guess {
+        pub fn new(value: i32) -> Guess {
+            if value < 1 {
+                panic!(
+                    "Guess value must be greater than or equal to 1, got {}.",
+                    value
+                );
+            } else if value > 100 {
+                panic!(
+                    "Guess value must be less than or equal to 100, got {}.",
+                    value
+                );
+            }
+    
+            Guess { value }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests { //CH11
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_works2() -> Result<(), String> {
+        if 2 + 2 == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
+    }
+
+    use super::testables::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+        assert!(larger.can_hold(&smaller));
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"),
+        "Greeting did not contain name, value was `{}`",
+        result);
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_panic(){
+        dont_panic();
+    }
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    #[ignore]
+    fn expensive_test() {
+        // code that takes an hour to run
+    }
+}
