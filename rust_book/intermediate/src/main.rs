6b91873097bf584186577733196296aec5b86c58
Josh Reuben <jreuben111@gmail.com>
2023-03-12 20:42:40 +0200

    iterators

--- rust_book/intermediate/src/main.rs
+++ rust_book/intermediate/src/main.rs
@@ -2,6 +2,7 @@
 
 fn main() {
     fp::closures();
+    fp::iterators();
 }
 
 mod fp { //CH13
@@ -100,9 +101,90 @@ mod fp { //CH13
         ];
 
         list4.sort_by_key(|r| r.width);
-        println!("{:#?}", list);
+        println!("{:#?}", list4);
 
     }
 
+    pub fn iterators(){
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+        for val in v1_iter {
+            println!("Got: {}", val);
+        }
+    }
 
+    #[derive(PartialEq, Debug)]
+    pub struct Shoe {
+        pub size: u32,
+        pub style: String,
+    }
+
+    pub fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+        shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+    }
+
+
+}
+#[cfg(test)]
+mod iterator_tests {
+    #[test]
+    fn manual_iterator() {
+        let v1 = vec![1, 2, 3];
+        let mut v1_iter = v1.iter();
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+        let total: i32 = v1_iter.sum();
+        assert_eq!(total, 6);
+    }
+
+    #[test]
+    fn iterator_map() {
+        let v1: Vec<i32> = vec![1, 2, 3];
+        let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+        assert_eq!(v2, vec![2, 3, 4]);
+    }
+
+    
+    #[test]
+    fn iterator_filter() {
+        use crate::fp::*;
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+        
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
 }
