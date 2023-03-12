9cc31fec9a43a6d58ea7ceb552a98ed18a05f801
Josh Reuben <jreuben111@gmail.com>
2023-03-12 06:41:27 +0200

    Config struct

--- rust_book/minigrep/src/main.rs
+++ rust_book/minigrep/src/main.rs
@@ -1,12 +1,41 @@
 use std::env;
-/// cargo run -- searchstring example-filename.txt
+use std::fs;
+
+/// cargo run -- searchstring poem.txt
 fn main() {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
 
-    let query = &args[1];
-    let file_path = &args[2];
+    // let (query, file_path) = parse_config(&args);
+    let config = Config::new(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
+
+// fn parse_config(args: &[String]) -> (&str, &str) {
+//     let query = &args[1];
+//     let file_path = &args[2];
+
+//     (query, file_path)
+// }
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
+}
+
+
