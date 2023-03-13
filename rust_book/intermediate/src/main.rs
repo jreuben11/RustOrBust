0bf5f9f8727dd125b7f0bade072239b30a5ec184
Josh Reuben <jreuben111@gmail.com>
2023-03-13 12:44:54 +0200

    circular_ref_prevention - WeakRef

--- rust_book/intermediate/src/main.rs
+++ rust_book/intermediate/src/main.rs
@@ -403,13 +403,36 @@ pub mod smart_pointers { //CH15
             children: RefCell::new(vec![]),
         });
         println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
 
-        let branch = Rc::new(Node {
-            value: 5,
-            parent: RefCell::new(Weak::new()),
-            children: RefCell::new(vec![Rc::clone(&leaf)]),
-        });
-        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        {
+            let branch = Rc::new(Node {
+                value: 5,
+                parent: RefCell::new(Weak::new()),
+                children: RefCell::new(vec![Rc::clone(&leaf)]),
+            });
+            *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+            println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+            println!(
+                "branch strong = {}, weak = {}",
+                Rc::strong_count(&branch),
+                Rc::weak_count(&branch),
+            );
+            println!(
+                "leaf strong = {}, weak = {}",
+                Rc::strong_count(&leaf),
+                Rc::weak_count(&leaf),
+            );
+        }
         println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
     }
 }
