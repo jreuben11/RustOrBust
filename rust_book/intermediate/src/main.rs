596d35474106b28248da1e25422be39784ee2951
Josh Reuben <jreuben111@gmail.com>
2023-03-16 20:05:42 +0200

    oop encapsulation and duck typing

--- rust_book/intermediate/src/main.rs
+++ rust_book/intermediate/src/main.rs
@@ -13,6 +13,9 @@ fn main() {
     concurrency::thread_spawn();
     concurrency::message_passing();
     concurrency::shared_state();
+
+    oop::encapsulation();
+    oop::duck_typing();
 }
 
 mod fp { //CH13
@@ -535,5 +538,106 @@ pub mod concurrency { //CH16
         println!("Result: {}", *counter.lock().unwrap());
     }
 
+}
+
+pub mod oop { //CH17
+    pub fn encapsulation (){
+        pub struct AveragedCollection {
+            list: Vec<i32>,
+            average: f64,
+        }
+        impl AveragedCollection {
+            pub fn add(&mut self, value: i32) {
+                self.list.push(value);
+                self.update_average();
+            }
+            pub fn remove(&mut self) -> Option<i32> {
+                let result = self.list.pop();
+                match result {
+                    Some(value) => {
+                        self.update_average();
+                        Some(value)
+                    }
+                    None => None,
+                }
+            }
+            pub fn average(&self) -> f64 {
+                self.average
+            }
+            fn update_average(&mut self) {
+                let total: i32 = self.list.iter().sum();
+                self.average = total as f64 / self.list.len() as f64;
+            }
+        }
+
+        let mut ac = AveragedCollection{list:vec!(), average:0f64};
+        for i in 1..5 {
+            ac.add(i);
+        }
+        println!("average:{}", ac.average());
+        
+
+
+    }
+
+    pub fn duck_typing (){
+        pub trait Draw {
+            fn draw(&self);
+        }
+        pub struct Screen {
+            pub components: Vec<Box<dyn Draw>>,
+        }
+        impl Screen {
+            pub fn run(&self) {
+                for component in self.components.iter() {
+                    component.draw();
+                }
+            }
+        }
+        #[derive(Debug)]
+        pub struct Button {
+            pub width: u32,
+            pub height: u32,
+            pub label: String,
+        }
+        impl Draw for Button {
+            fn draw(&self) {
+                println!("draw: {:?}", &self);
+            }
+        }
+        #[derive(Debug)]
+        struct SelectBox {
+            width: u32,
+            height: u32,
+            options: Vec<String>,
+        } 
+        impl Draw for SelectBox {
+            fn draw(&self) {
+                println!("draw: {:?}", &self);
+            }
+        }
+
+        let screen = Screen {
+            components: vec![
+                Box::new(SelectBox {
+                    width: 75,
+                    height: 10,
+                    options: vec![
+                        String::from("Yes"),
+                        String::from("Maybe"),
+                        String::from("No"),
+                    ],
+                }),
+                Box::new(Button {
+                    width: 50,
+                    height: 10,
+                    label: String::from("OK"),
+                }),
+            ],
+        };
+    
+        screen.run();
+
+    }
 }
 
