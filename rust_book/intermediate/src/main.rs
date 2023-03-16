96cc667e812277328c719554539a783d73d9f2dc
Josh Reuben <jreuben111@gmail.com>
2023-03-16 20:26:40 +0200

    states_as_types

--- rust_book/intermediate/src/main.rs
+++ rust_book/intermediate/src/main.rs
@@ -16,6 +16,7 @@ fn main() {
 
     oop::encapsulation();
     oop::duck_typing();
+    oop::states_as_types();
 }
 
 mod fp { //CH13
@@ -638,6 +639,55 @@ pub mod oop { //CH17
     
         screen.run();
 
+    }
+
+    pub fn states_as_types(){
+        pub struct Post {
+            content: String,
+        }
+        pub struct DraftPost {
+            content: String,
+        }
+        pub struct PendingReviewPost {
+            content: String,
+        }
+        
+        
+        impl Post {
+            pub fn new() -> DraftPost {
+                DraftPost {
+                    content: String::new(),
+                }
+            }
+            pub fn content(&self) -> &str {
+                &self.content
+            }
+        }
+        
+        impl DraftPost {
+            pub fn add_text(&mut self, text: &str) {
+                self.content.push_str(text);
+            }
+            pub fn request_review(self) -> PendingReviewPost {
+                PendingReviewPost {
+                    content: self.content,
+                }
+            }
+        }
+
+        impl PendingReviewPost {
+            pub fn approve(self) -> Post {
+                Post {
+                    content: self.content,
+                }
+            }
+        }
+
+        let mut post = Post::new();
+        post.add_text("I ate a salad for lunch today");
+        let post = post.request_review();
+        let post = post.approve();
+        assert_eq!("I ate a salad for lunch today", post.content());
     }
 }
 
