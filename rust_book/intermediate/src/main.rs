37f108ad91dff8600f174404070b4b337ed522c5
Josh Reuben <jreuben111@gmail.com>
2023-03-17 09:03:46 +0200

    unsafe

--- rust_book/intermediate/src/main.rs
+++ rust_book/intermediate/src/main.rs
@@ -19,6 +19,8 @@ fn main() {
     oop::states_as_types();
 
     pattern_matching::patterns_everywhere();
+
+    advanced::safety_off();
 }
 
 mod fp { //CH13
@@ -818,7 +820,77 @@ pub mod pattern_matching { //CH18
         }
         
     }
+  
+}
+
+pub mod advanced { // CH19
+
+    pub fn safety_off(){
+
+        let mut num = 5;
+        let r1 = &num as *const i32;
+        let r2 = &mut num as *mut i32;
+        let r3 = 0x012345usize as *const i32;
+        unsafe {
+            println!("r1 is: {}", *r1);
+            println!("r2 is: {}", *r2);
+        }
+
+        unsafe fn dangerous() {}
+        unsafe {
+            dangerous();
+        }
+
+        fn split_at_mut(values: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
+            use std::slice;
+            let len = values.len();
+            let ptr = values.as_mut_ptr();
+            assert!(mid <= len);
+        
+            unsafe {
+                (
+                    slice::from_raw_parts_mut(ptr, mid),
+                    slice::from_raw_parts_mut(ptr.add(mid), len - mid),
+                )
+            }
+        }
+        let mut v = vec![1, 2, 3, 4, 5, 6];
+        let r = &mut v[..];
+        let (a, b) = split_at_mut(r, 3);
+        assert_eq!(a, &mut [1, 2, 3]);
+        assert_eq!(b, &mut [4, 5, 6]);
+
+        extern "C" {
+            fn abs(input: i32) -> i32;
+        }
+        unsafe {
+            println!("Absolute value of -3 according to C: {}", abs(-3));
+        }
+
+        #[no_mangle]
+        pub extern "C" fn call_from_c() {
+            println!("Just called a Rust function from C!");
+        }
+
+        static mut COUNTER: u32 = 0;
+        fn add_to_count(inc: u32) {
+            unsafe {
+                COUNTER += inc;
+            }
+        }
+        add_to_count(3);
+        unsafe {
+            println!("COUNTER: {}", COUNTER);
+        }
+
+        unsafe trait Foo {
+            // methods go here
+        }
+        unsafe impl Foo for i32 {
+            // method implementations go here
+        }
+    }
 
     
-    
+
 }
