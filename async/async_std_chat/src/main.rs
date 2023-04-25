bab731ac33aa719d6614c6dc28a899512bac844a
Josh Reuben <jreuben111@gmail.com>
2023-04-25 16:16:39 +0300

    clean shutdown

--- async/async_std_chat/src/main.rs
+++ async/async_std_chat/src/main.rs
@@ -22,38 +22,45 @@ fn run() -> Result<()> {
 async fn accept_loop(addr: impl ToSocketAddrs) -> Result<()> { 
 
     let listener = TcpListener::bind(addr).await?; 
-    let (broker_sender, broker_receiver) = mpsc::unbounded();
+    let (broker_sender, broker_receiver) = mpsc::unbounded();   // 1
     let broker_handle = task::spawn(broker_loop(broker_receiver));
     let mut incoming = listener.incoming();
     while let Some(stream) = incoming.next().await { 
         let stream = stream?;
         println!("Accepting from: {}", stream.peer_addr()?);
-        let _handle = task::spawn(connection_loop(stream));
-        // spawn_and_log_error(connection_loop(broker_sender.clone(), stream));
+        spawn_and_log_error(connection_loop(broker_sender.clone(), stream));
     }
     drop(broker_sender); // 1
     broker_handle.await?; // 5
     Ok(())
 }
 
-async fn connection_loop(stream: TcpStream) -> Result<()> {
-    let reader = BufReader::new(&stream); // 2
+async fn connection_loop(mut broker: Sender<Event>, stream: TcpStream) -> Result<()> {
+
+    let stream = Arc::new(stream); 
+    let reader = BufReader::new(&*stream); // 2
     let mut lines = reader.lines();
 
-    let name = match lines.next().await { // 3
+    let name = match lines.next().await { 
         None => Err("peer disconnected immediately")?,
         Some(line) => line?,
     };
-    println!("name = {}", name);
+    broker.send(Event::NewPeer { name: name.clone(), stream: Arc::clone(&stream) }).await // 3
+        .unwrap();
 
-    while let Some(line) = lines.next().await { // 4
+    while let Some(line) = lines.next().await { 
         let line = line?;
-        let (dest, msg) = match line.find(':') { // 5
+        let (dest, msg) = match line.find(':') { 
             None => continue,
             Some(idx) => (&line[..idx], line[idx + 1 ..].trim()),
         };
         let dest: Vec<String> = dest.split(',').map(|name| name.trim().to_string()).collect();
         let msg: String = msg.to_string();
+        broker.send(Event::Message { // 4
+            from: name.clone(),
+            to: dest,
+            msg,
+        }).await.unwrap();
     }
     Ok(())
 }
@@ -103,7 +110,8 @@ enum Event { // 1
 }
 
 async fn broker_loop(mut events: Receiver<Event>) -> Result<()> {
-    let mut peers: HashMap<String, Sender<String>> = HashMap::new(); // 2
+    let mut writers = Vec::new();
+    let mut peers: HashMap<String, Sender<String>> = HashMap::new(); 
 
     while let Some(event) = events.next().await {
         match event {
@@ -121,11 +129,16 @@ async fn broker_loop(mut events: Receiver<Event>) -> Result<()> {
                     Entry::Vacant(entry) => {
                         let (client_sender, client_receiver) = mpsc::unbounded();
                         entry.insert(client_sender); // 4
-                        spawn_and_log_error(connection_writer_loop(client_receiver, stream)); // 5
+                        let handle = spawn_and_log_error(connection_writer_loop(client_receiver, stream));
+                        writers.push(handle);
                     }
                 }
             }
         }
+    }
+    drop(peers); // 3
+    for writer in writers { // 4
+        writer.await;
     }
     Ok(())
 }
