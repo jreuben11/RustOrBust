d0d41d0afbd8e28ae214a180721b6080aa6244a1
Josh Reuben <jreuben111@gmail.com>
2024-03-15 11:06:35 +0200

    tests + doubly linked list

--- data_structures_and_algos/README.md
+++ data_structures_and_algos/README.md
@@ -1,11 +1,28 @@
 source: https://github.com/PacktPublishing/Hands-On-Data-Structures-and-Algorithms-with-Rust
-
-# to investigate:
-- String.to_owned
-- borrow_mut
-- Rc::try_unwrap(x)
+# 1. refresher
+- [sync::mpsc](ch1_refresher/src/main.rs)
+# 2. cargo refreher
+- [doc tests](ch2_crate_test/src/lib.rs)
+- [cargo config](.cargo/config)
+- [cargo workspace](Cargo.toml)
+# 3. efficient storage refresher
+- [mem::size_of](ch3_efficient_storage/src/main.rs)
+- `Sized` / `?Sized`
+# 4. lists
+- [bencher](ch4_lists/benches/example.rs)
+- [tests](ch4_lists/src/lib.rs)
+- [single linked list](ch4_lists/src/singly_linked_list.rs)
+  - `type Link = Option<Rc<RefCell<Node>>>`
+  - `String.to_owned`
+  - `borrow_mut`
+  - `Rc::try_unwrap(x)
                 .ok()
                 .expect("xxx")
                 .into_inner()
-                .value
-- Sized / ?Sized
+                .value`
+  - LinkedList
+  - `Option.take()` https://doc.rust-lang.org/std/option/enum.Option.html#method.take 
+  - `mem::replace()` https://doc.rust-lang.org/stable/std/mem/fn.replace.html
+- [double linked list](ch4_lists/src/doubly_linked_list.rs)
+  - implementing `iter` https://doc.rust-lang.org/std/iter/index.html#implementing-iterator `Iterator`, `IntoIterator`, `DoubleEndedIterator`
+  - borrow
--- data_structures_and_algos/ch4_lists/src/lib.rs
+++ data_structures_and_algos/ch4_lists/src/lib.rs
@@ -1,4 +1,5 @@
 mod singly_linked_list;
+mod doubly_linked_list;
 
 #[cfg(test)]
 mod tests {
@@ -48,6 +49,90 @@ mod tests {
             Some("INSERT INTO mytable VALUES (1,2,3)".to_owned())
         );
         assert_eq!(list.pop(), None);
+    }
+
+
+    #[test]
+    fn better_transaction_log_append() {
+        let mut transaction_log = doubly_linked_list::BetterTransactionLog::new_empty();
+        assert_eq!(transaction_log.length, 0);
+        transaction_log.append("INSERT INTO mytable VALUES (1,2,3)".to_owned());
+        transaction_log.append("INSERT INTO mytable VALUES (2,3,4)".to_owned());
+        transaction_log.append("INSERT INTO mytable VALUES (3,4,5)".to_owned());
+        assert_eq!(transaction_log.length, 3);
+        assert_eq!(
+            transaction_log.pop(),
+            Some("INSERT INTO mytable VALUES (1,2,3)".to_owned())
+        );
+        assert_eq!(
+            transaction_log.pop(),
+            Some("INSERT INTO mytable VALUES (2,3,4)".to_owned())
+        );
+        assert_eq!(
+            transaction_log.pop(),
+            Some("INSERT INTO mytable VALUES (3,4,5)".to_owned())
+        );
+        assert_eq!(transaction_log.pop(), None);
+    }
+
+
+    #[test]
+    fn better_transaction_log_pop() {
+        let mut list = doubly_linked_list::BetterTransactionLog::new_empty();
+        assert_eq!(list.pop(), None);
+        list.append("INSERT INTO mytable VALUES (1,2,3)".to_owned());
+        list.append("INSERT INTO mytable VALUES (1,2,3)".to_owned());
+        list.append("INSERT INTO mytable VALUES (1,2,3)".to_owned());
+        assert_eq!(
+            list.pop(),
+            Some("INSERT INTO mytable VALUES (1,2,3)".to_owned())
+        );
+        assert_eq!(
+            list.pop(),
+            Some("INSERT INTO mytable VALUES (1,2,3)".to_owned())
+        );
+        assert_eq!(
+            list.pop(),
+            Some("INSERT INTO mytable VALUES (1,2,3)".to_owned())
+        );
+        assert_eq!(list.pop(), None);
+    }
+
+
+    #[test]
+    fn better_transaction_log_iterator() {
+        let mut list = doubly_linked_list::BetterTransactionLog::new_empty();
+        assert_eq!(list.pop(), None);
+        list.append("INSERT INTO mytable VALUES (1,2,3)".to_owned());
+        list.append("INSERT INTO mytable VALUES (2,3,4)".to_owned());
+        list.append("INSERT INTO mytable VALUES (3,4,5)".to_owned());
+        let mut iter = list.clone().into_iter();
+        assert_eq!(
+            iter.next(),
+            Some("INSERT INTO mytable VALUES (1,2,3)".to_owned())
+        );
+        assert_eq!(
+            iter.next(),
+            Some("INSERT INTO mytable VALUES (2,3,4)".to_owned())
+        );
+        assert_eq!(
+            iter.next(),
+            Some("INSERT INTO mytable VALUES (3,4,5)".to_owned())
+        );
+
+        let mut iter = list.clone().back_iter();
+        assert_eq!(
+            iter.next_back(),
+            Some("INSERT INTO mytable VALUES (3,4,5)".to_owned())
+        );
+        assert_eq!(
+            iter.next_back(),
+            Some("INSERT INTO mytable VALUES (2,3,4)".to_owned())
+        );
+        assert_eq!(
+            iter.next_back(),
+            Some("INSERT INTO mytable VALUES (1,2,3)".to_owned())
+        );
     }
 
 }
