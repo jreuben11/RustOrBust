c23fc5602890f371f0098e24576a81c9da41db00
Josh Reuben <jreuben111@gmail.com>
2024-04-06 09:51:25 +0300

    tokio shared state

--- tokio/my-redis/src/main.rs
+++ tokio/my-redis/src/main.rs
@@ -1,46 +1,53 @@
 use tokio::net::{TcpListener, TcpStream};
-use mini_redis::{Connection, Frame};
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    println!("Listening");
+    let db = Arc::new(Mutex::new(HashMap::new()));
 
     loop {
         // The second item contains the IP and port of the new connection.
         let (socket, _) = listener.accept().await.unwrap();
+
+        // Clone the handle to the hash map.
+        let db = db.clone();
+
         // process(socket).await;
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
-    use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
+async fn process(socket: TcpStream, db:Db) {
+    use mini_redis::{Connection, Frame};
+    use mini_redis::Command::{self, Get, Set};    
 
-    // A hashmap is used to store data
-    let mut db = HashMap::new();
-
-    // Connection, provided by `mini-redis`, handles parsing frames from
-    // the socket
+    // Connection, provided by `mini-redis`, handles parsing frames from the socket
     let mut connection = Connection::new(socket);
 
     // Use `read_frame` to receive a command from the connection.
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                // The value is stored as `Vec<u8>`
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                // db.insert(cmd.key().to_string(), cmd.value().to_vec()); // The value is no longer stored as `Vec<u8>`
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    // `Frame::Bulk` expects data to be of type `Bytes`. This
-                    // type will be covered later in the tutorial. For now,
-                    // `&Vec<u8>` is converted to `Bytes` using `into()`.
-                    Frame::Bulk(value.clone().into())
+                    // `Frame::Bulk` expects data to be of type `Bytes`. 
+                    Frame::Bulk(value.clone())
+                    // Frame::Bulk(value.clone().into()) // `&Vec<u8>` no longer converted to `Bytes` using `into()`.
                 } else {
                     Frame::Null
                 }
