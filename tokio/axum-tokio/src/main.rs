1b5ae394c1dedef509cc659db76593262de9ad0b
Josh Reuben <jreuben111@gmail.com>
2024-04-17 08:57:52 +0300

    axum tokio - fallback

--- tokio/axum-tokio/src/main.rs
+++ tokio/axum-tokio/src/main.rs
@@ -3,11 +3,24 @@
 pub async fn main() {
      // Build our application by creating our router.
     let app = axum::Router::new()
-        .route("/",
-            axum::routing::get(|| async { "Hello, World!" })
-        );
+        .fallback(fallback)
+        .route("/",  axum::routing::get(|| async { "default!" }))
+        .route("/hello",get(hello));
 
     // Run our application as a hyper server on http://localhost:3000.
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
+
+/// axum handler for any request that fails to match the router routes.
+/// This implementation returns HTTP status code Not Found (404).
+pub async fn fallback(uri: axum::http::Uri) -> impl axum::response::IntoResponse {
+    (axum::http::StatusCode::NOT_FOUND, format!("No route {}", uri))
+}
+
+/// axum handler for "GET /" which returns a string and causes axum to
+/// immediately respond with status code `200 OK` and with the string.
+pub async fn hello() -> String {
+    "Hello, World!".to_string()
+}
+
