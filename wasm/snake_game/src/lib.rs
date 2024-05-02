7a7043c6b9fcba66f93de80d1f3ce5e93cb03862
Josh Reuben <jreuben111@gmail.com>
2024-05-02 18:52:51 +0300

    world state + render canvas

--- wasm/snake_game/src/lib.rs
+++ wasm/snake_game/src/lib.rs
@@ -1,11 +1,51 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(name);
+// #[wasm_bindgen]
+// pub fn greet(name: &str) {
+//     alert(name);
+// }
+
+// #[wasm_bindgen]
+// extern {
+//     pub fn alert(s: &str);
+// }
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>
 }
 
+impl Snake {
+    fn new(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec!(SnakeCell(spawn_index))
+        }
+    }
+}
+
+
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+pub struct World {
+    width: usize,
+    snake: Snake,
+}
+
+
+#[wasm_bindgen]
+impl World {
+    pub fn new() -> World {
+        World {
+            width: 8,
+            snake: Snake::new(10)
+        }
+    }
+
+    pub fn width(&self) -> usize {
+        self.width
+    }
+
+    pub fn snake_head_idx(&self) -> usize {
+        self.snake.body[0].0
+     }
 }
--- wasm/snake_game/www/index.js
+++ wasm/snake_game/www/index.js
@@ -1,6 +1,48 @@
-import init, { greet } from "snake_game";
+import init, { World } from "snake_game";
 
 init().then(_ => {
-  greet("Filip");
-  console.log("OK!");
+  const CELL_SIZE = 50;
+  const world = World.new();
+  const worldWidth = world.width();
+
+  const canvas = document.getElementById("snake-canvas");
+  const ctx = canvas.getContext("2d");
+
+  canvas.height = worldWidth * CELL_SIZE;
+  canvas.width = worldWidth * CELL_SIZE;
+
+  function drawWorld() {
+    ctx.beginPath();
+
+    for (let x = 0; x < worldWidth + 1; x++) {
+      ctx.moveTo(CELL_SIZE * x, 0);
+      ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE)
+    }
+
+    for (let y = 0; y < worldWidth + 1; y++) {
+      ctx.moveTo(0, CELL_SIZE * y);
+      ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y)
+    }
+
+    ctx.stroke();
+  }
+
+  function drawSnake() {
+    const snakeIdx = world.snake_head_idx();
+    const col = snakeIdx % worldWidth;
+    const row = Math.floor(snakeIdx / worldWidth);
+
+    ctx.beginPath();
+    ctx.fillRect(
+      col * CELL_SIZE,
+      row * CELL_SIZE,
+      CELL_SIZE,
+      CELL_SIZE
+    );
+    ctx.stroke();
+  }
+
+  console.log(world.snake_head_idx());
+  drawWorld();
+  drawSnake();
 })
